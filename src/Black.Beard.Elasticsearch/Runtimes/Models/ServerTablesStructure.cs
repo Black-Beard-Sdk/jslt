4191653f99aa08f2f8577d57f3c68f4140d7fac7
gael beard <g.beard@pickup-services.com>
2023-06-30 10:33:22 +0200

    Update packages contentLoaders and roslyn

--- src/Black.Beard.Elasticsearch/Runtimes/Models/ServerTablesStructure.cs
+++ src/Black.Beard.Elasticsearch/Runtimes/Models/ServerTablesStructure.cs
@@ -61,7 +61,7 @@ namespace Bb.Elastic.Runtimes.Models
             if (string.IsNullOrEmpty(this.Filename))
                 throw new InvalidOperationException("missing name for loading");
 
-            var sb = ContentHelperFiles.LoadFromFile(this.Filename);
+            var sb = this.Filename.LoadFromFile();
             JArray a = JArray.Parse(sb.ToString());
 
             this.Deserialize(a);
