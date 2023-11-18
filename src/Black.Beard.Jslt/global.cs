57de5de71e6c45bb0f1833b9aa19d744ac1f43cb
gael beard <gaelgael5@gmail.com>
2023-11-18 18:36:39 +0100

    test 7

---
+++ src/Black.Beard.Jslt/global.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+[assembly: SuppressMessage("Usage", "CS8632:The annotation for nullable reference types should only be used in code within a '#nullable' annotations context", Justification = "Not production code.", Scope = "member", Target = "~M:MyApp.Program.IgnorableCharacters")]
